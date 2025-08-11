9aec3b938a9e1a0332f83b21deb927b8dfdc781c
yangwei999 <348134071@qq.com>
2025-08-11 16:48:20 +0800

    optimize

--- scanning/app/config.go
+++ scanning/app/config.go
@@ -5,3 +5,17 @@ type TrivyRepo struct {
 	TrivyDB  string `json:"trivy_db"  required:"true"`
 	VulnList string `json:"vuln_list" required:"true"`
 }
+
+func (t *TrivyRepo) SetDefault() {
+	if t.Trivy == "" {
+		t.Trivy = "https://github.com/wjunLu/trivy.git"
+	}
+
+	if t.TrivyDB == "" {
+		t.TrivyDB = "https://github.com/wjunLu/trivy-db.git"
+	}
+
+	if t.VulnList == "" {
+		t.VulnList = "https://github.com/aquasecurity/vuln-list.git"
+	}
+}
